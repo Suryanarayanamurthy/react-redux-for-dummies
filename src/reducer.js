7427a49d8fbe2997b0a8cb3ff0767cff8b934b64
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2017-01-14 21:53:28 +0100

    add reducer to set visibility filter

--- src/reducer.js
+++ src/reducer.js
@@ -6,7 +6,12 @@ const initialState = {
 }
 
 function todoApp(state = initialState, action) {
-  // For now, don't handle any actions
-  // and just return the state given to us.
-  return state
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    default:
+      return state
+  }
 }
