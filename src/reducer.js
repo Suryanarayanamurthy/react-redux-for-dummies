bdcfd4ae51993c112b127448d2f8405469e4a585
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2017-01-15 19:38:53 +0100

    split reducers to update todos and visiability todos_list

--- src/reducer.js
+++ src/reducer.js
@@ -5,33 +5,41 @@ const initialState = {
   todos: []
 }
 
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
 function todoApp(state = initialState, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return Object.assign({}, state, {
         visibilityFilter: action.filter
       })
-	case ADD_TODO:
+    case ADD_TODO:
+    case TOGGLE_TODO:
       return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
+        todos: todos(state.todos, action)
       })
-	case TOGGLE_TODO:
-	  return Object.assign({}, state, {
-	    todos: state.todos.map((todo, index) => {
-	      if (index === action.index) {
-	        return Object.assign({}, todo, {
-	          completed: !todo.completed
-	        })
-	      }
-	      return todo
-	    })
-	  })               	
     default:
       return state
   }
